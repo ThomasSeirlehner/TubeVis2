550c4ec54f6d6b8824b98e14599a55e9d9811c53
ThomasSeirlehner <e12022486@student.tuwien.ac.at>
2024-10-31 14:29:24 +0100

    partly fixing compute shader

--- TubeVis2/TubeVis2/Content/ComputeShader.hlsl
+++ TubeVis2/TubeVis2/Content/ComputeShader.hlsl
@@ -1,7 +1,12 @@
-RWTexture2D<float4> targetTexture : register(u0);
+RWTexture2D<float4> buffer : register(u0);
 
-[numthreads(1, 1, 1)]
+[numthreads(8, 8, 1)]
 void main(uint3 dispatchThreadID : SV_DispatchThreadID)
 {
-    targetTexture[dispatchThreadID.xy] = float4(0.0f, 0.0f, 0.0f, 1.0f);
+    int width;
+    int height;
+    buffer.GetDimensions(width, height);
+    
+    float2 uv = dispatchThreadID.xy / float2(width, height);
+    buffer[dispatchThreadID.xy] = float4(uv.xy, 0.0f, 1.0f);
 }
